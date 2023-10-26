9568281106311e52a41074f91b3c22c963d1c8b5
Lodestar <122153472+ITlodestar@users.noreply.github.com>
2023-10-26 21:01:30 +0900

    add indext.js

---
+++ src/index copy.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+// import styling
+import "./assets/css/common.css";
+import "./assets/css/hero.css";
+import "./assets/css/navbar.css";
+import "./assets/css/trading-view.css";
+import "./assets/css/trading-dashboard.css";
+import "./assets/css/search-input.css";
+import "./assets/css/mobile-section.css";
+import "./assets/css/profile.css";
+import "./assets/css/popup.css";
+import "./assets/css/mobile-leaderboard.css";
+
+// import App.js
+import App from './App';
+
+import ThemeContextProvider from './contexts/ThemeContext';
+
+ReactDOM.render(
+  <ThemeContextProvider>
+    <App />
+  </ThemeContextProvider>,
+  document.getElementById('root')
+);
